376303d29987689397e278592c45ef3bcb98e4e4
lucyrush <lucy@codeinstitute.net>
2020-12-07 18:46:36 +0000

    Added blog article details

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-# from . import views
-from.views import Blog
+from.views import Blog, ArticleDetail
 
 
 urlpatterns = [
-    path('', Blog.as_view(), name="blog")
+    path('', Blog.as_view(), name="blog"),
+    path('article/<int:pk>', ArticleDetail.as_view(), name="article-detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DeleteView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
 class Blog(ListView):
     model = Post
-    template_name ='blog/blog.html'
+    template_name = 'blog/blog.html'
+
+
+class ArticleDetail(DetailView):
+    model = Post
+    template_name = 'blog/article-details.html'
