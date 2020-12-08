422756431de5de86fc9b0993633a8661d578bbcf
lucyrush <lucy@codeinstitute.net>
2020-12-08 18:49:29 +0000

    Added new blog model

--- blog/models.py
+++ blog/models.py
@@ -1,21 +1,18 @@
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField(blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
     def __str__(self):
-        return self.title + ' | ' + str(self.author)
-
-
-class Comment(models.Model):
-    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    body = models.TextField()
-    date_added = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return '%s - %s' % (self.post.title, self.name)
+        return self.title
--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,1 @@
-from django.urls import path
-from.views import Blog, ArticleDetail
 
-
-urlpatterns = [
-    path('', Blog.as_view(), name="blog"),
-    path('article/<int:pk>', ArticleDetail.as_view(), name="article-detail"),
-]
